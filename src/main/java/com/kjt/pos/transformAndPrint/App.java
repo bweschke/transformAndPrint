d77dc33a1424e763f089fea19669d5a5d3eb4ade
BJ Weschke <bweschke@getskt.com>
2021-08-18 14:09:56 -0400

    test app

--- src/main/java/com/kjt/pos/transformAndPrint/App.java
+++ src/main/java/com/kjt/pos/transformAndPrint/App.java
@@ -1,13 +1,28 @@
 package com.kjt.pos.transformAndPrint;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	final static jLpr jlpr = new jLpr();
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+    	try {
+    		//jlpr.printFile(fileName, hostName, PASSTHRU);
+			jlpr.printFile(args[0].toString(), args[1].toString(), args[2].toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
